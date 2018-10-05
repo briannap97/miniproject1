a103c1e41b488dcbaef822ff7fe4da0537ca5bcf
briannap <bp275@njit.edu>
2018-10-05 13:24:03 -0400

    Task: Created gitignore file

--- public/index.php
+++ public/index.php
@@ -5,4 +5,43 @@
  * Date: 10/2/18
  * Time: 9:01 PM
  */
-echo 'test12345';
+
+main::start("example.csv");
+
+class main {
+
+    static public function start() {
+        $records= csv::getRecords();
+        $table =html::generateTable($records);
+        system::printPage($table);
+
+
+    }
+}
+
+class csv {
+
+    static public function getRecords() {
+
+    }
+}
+
+class html {
+    static public function generateTable($records) {
+
+        $table = 'test';
+
+        return $table;
+    }
+}
+
+
+
+
+class system {
+    static public function printPage($page) {
+
+        echo $page;
+
+    }
+}
